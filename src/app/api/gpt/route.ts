d27c55018a3817dfce3c7f4048ce3abdb4a46e72
rohank36 <rohan.kanti@mail.mcgill.ca>
2024-09-01 13:19:48 -0400

    feat/gpt-summary

---
+++ src/app/api/gpt/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server';
+import OpenAI from "openai";
+
+export async function POST(req: NextRequest, res: NextResponse){
+    try{
+        const body = await req.json();
+        const { transcript } = body;
+        if(!transcript){    
+            return NextResponse.json({ error: 'No transcript provided', status: 400 });
+        }
+        const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+        const sys_message = "You are an expert summarizer whose only task is to summarize the given transcripts to the best of your ability in one sentence followed by several bullet points."
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "system", content: sys_message }, { role: "user", content: transcript }],
+            model: "gpt-4o",
+        });
+        const summary = completion.choices[0].message.content;
+        if (summary){
+            return NextResponse.json({ summary: summary, status: 200 });
+        }else{
+            return NextResponse.json({ error: "Summary not found in Server", status: 500});
+        }
+        
+    }catch(error){  
+        console.error("Server error creating summary:", error);
+        return NextResponse.json({ error: 'Server error creating summary', status: 500 });
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,82 @@ import React, {useState} from "react";
 
 export default function Home() {
   const [url, setUrl] = useState<string>("");
-  const [transcript, setTranscript] = useState<string>("");
+  const [summary, setSummary] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleSubmit = async (e:any) =>{
     e.preventDefault();
-    console.log(url);
+    setLoading(true)
+    const transcript = await getTranscript(url);
+    await getSummary(transcript);
+    setLoading(false)
+  }
 
+  const getTranscript = async (url:string) => {
     try{
       const res = await fetch(`/api/summary?video_id=${url}`)
       const data = await res.json();
       if (res.ok){
-        setTranscript(data.transcript);
-        console.log(data.transcript);
+        console.log("Transcript fetched successfully");
+        return data.transcript;
+      }else{
+        return null;
       }
       
     }catch(error){
       console.error("Error fetching transcript:", error);
+      return null;
     }
-
   }
+
+  const getSummary = async (transcript:string) => {
+    try{
+
+      if(!transcript){
+        console.error("Cannot get summary because no transcript was found");
+        return;
+      }
+
+      const res = await fetch(`/api/gpt`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ transcript }),
+      });
+
+      if(res.ok){
+        const data = await res.json();
+        setSummary(data.summary);
+        console.log("Summary fetched successfully");
+      }
+    }catch(error){
+      console.error("Error fetching summary:", error);
+    }
+  }
+
   return (
     <main>
       <div className="flex-col justify-center items-center min-h-screen">
+        
         <form className="flex gap-4 w-1/2" onSubmit={handleSubmit}>
           <label>Paste youtube url: </label>
           <input type="text" id="url" value={url} onChange={(e) => setUrl(e.target.value)} className="w-full"/>
-          <button>Summarize</button>
+          <button type="submit" disabled={loading}>Summarize</button>
         </form>
+        
+        <div>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            summary && (
+              <div>
+                <p>{summary}</p>
+              </div>
+            )
+          )}
+        </div>
+      
       </div>
     </main>
   );
